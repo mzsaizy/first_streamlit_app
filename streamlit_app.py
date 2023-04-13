8d450aa89b9b2471f5b4b9f340dbd834d8da6623
mzsaizy <47593638+mzsaizy@users.noreply.github.com>
2023-04-13 12:14:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ if streamlit.button('Get Fruit Load List'):
     streamlit.dataframe(my_data_rows)
 
 #don't run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 #import snowflake.connector
 
